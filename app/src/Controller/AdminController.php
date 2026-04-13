53636a66c96037251f1073bbf62c7fa672e24e19
Zem47 <zem47@lenovohaxman.localdomain>
2026-04-13 19:18:30 +0200

    exercice 10 en cours

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Sentence;
+use App\Form\SentenceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,11 +14,14 @@ final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin_index')]
     public function index(): Response
-         
-    {    $this->denyAccessUnlessGranted('ROLE_ADMIN');
-    
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
+
+  
 }
+    
--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -1,49 +1,87 @@
 <?php
+
+
 namespace App\Controller;
 
-use App\Entity\Sentence;
-use App\Repository\SentenceRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Entity\Sentence;
+use App\Repository\CommentRepository;
+use App\Repository\SentenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(SentenceRepository $sentencerepository): Response
-    {    
-        $sentences = $sentencerepository->findBy([], ['createdAT' => 'DESC']);
+    public function index(SentenceRepository $sentenceRepository): Response
+    {
+        $sentences = $sentenceRepository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('home/index.html.twig', [
             'sentences' => $sentences,
         ]);
     }
 
-    #[Route('/sentence/{id}', name: 'app_sentence_show')]
-    public function show(Sentence $sentence): Response
-    {
-        return $this->render('home/show.html.twig', [
-            'sentence' => $sentence,
+   #[Route('/sentence/{id}', name: 'app_sentence_show')]
+public function show(
+    int $id,
+    SentenceRepository $sentenceRepository,
+    CommentRepository $commentRepository,
+    Request $request,
+    EntityManagerInterface $em
+): Response {
+    $sentence = $sentenceRepository->find($id);
+
+    if (!$sentence) {
+        throw $this->createNotFoundException('Phrase introuvable.');
+    }
+
+    $comments = $commentRepository->findBy(
+        ['sentence' => $sentence],
+        ['createdAt' => 'DESC']
+    );
+
+    $comment = new Comment();
+    $form = $this->createForm(CommentType::class, $comment);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $comment->setAuthor($this->getUser());
+        $comment->setSentence($sentence);
+        $comment->setCreatedAt(new \DateTimeImmutable());
+        if (!$this->getUser()) {
+        throw $this->createAccessDeniedException();
+       }
+
+        $em->persist($comment);
+        $em->flush();
+
+        return $this->redirectToRoute('app_sentence_show', [
+            'id' => $sentence->getId(),
         ]);
     }
 
-
-    #[Route('/likes/{id}',name:'app_like_likes')]
-     public function likes(Sentence $sentence, EntityManagerInterface $em): Response
-     {   $sentence->setLikes($sentence->getLikes()+1);
-         $em->flush();
-         return $this->redirectToRoute('app_home',[
-            'id'=>$sentence->getId()
-         ]);
-        
-
-     }
-
-     
-
-    
+    return $this->render('home/show.html.twig', [
+        'sentence' => $sentence,
+        'comments' => $comments,
+        'form' => $form->createView(),
+    ]);
 }
-
-
-        
     
+
+    #[Route('/likes/{id}', name: 'app_like_likes')]
+    public function likes(Sentence $sentence, EntityManagerInterface $em): Response
+    {
+        $sentence->setLikes($sentence->getLikes() + 1);
+        $em->flush();
+
+        return $this->redirectToRoute('app_sentence_show', [
+            'id' => $sentence->getId(),
+        ]);
+    }
+
+}
