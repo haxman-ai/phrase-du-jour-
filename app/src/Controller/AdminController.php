4db75debb6d1275d9c5e2ea0063c38bdb2118369
Zem47 <zem47@lenovohaxman.localdomain>
2026-04-13 20:57:32 +0200

    feat: ajout espace administration (étape 9)

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -1,27 +1,25 @@
 <?php
-
 namespace App\Controller;
 
-use App\Entity\Sentence;
-use App\Form\SentenceType;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\SentenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin_index')]
-    public function index(): Response
+    public function index(SentenceRepository $sentenceRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
+        $sentences = $sentenceRepository->findBy(
+            [],
+            ['createdAt' => 'DESC']
+        );
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'sentences' => $sentences,
         ]);
     }
-
-  
 }
-    
